eee99580ec57b5142d3b4643c3906ce63ddcd4dd
Weston Kostura <weston.kostura@gmail.com>
2023-09-08 15:38:12 -0700

    icon and fixed results layout

--- Assets/script.js
+++ Assets/script.js
@@ -27,74 +27,82 @@ function currentTemp(citySearch) {
         .then(function (data) {
           console.log(data);
 
-          var city = citySearch.replace('%',' ');
+          var city = citySearch.replace("%", " ");
           var date = data.list[0].dt_txt;
           var currentTemp = data.list[0].main.temp;
           var tempF = ((currentTemp - 273.15) * 9) / 5 + 32;
           var wind = data.list[0].wind.speed;
           var humidity = data.list[0].main.humidity;
-          var icon = data.list[0].weather[0].icon
-          var iconUrl = `http://openweathermap.org/img/w/${icon}.png`
 
           function capitalizeWords(str) {
             const words = str.split(" ");
-            const capitalizedWords = words.map(word => {
+            const capitalizedWords = words.map((word) => {
               const firstLetter = word.charAt(0).toUpperCase();
               const restOfWord = word.slice(1);
               return firstLetter + restOfWord;
             });
             const capitalizedStr = capitalizedWords.join(" ");
             return capitalizedStr;
-          } 
-          
-          var fixedCurrentDate = date.split(" ")
+          }
+
+          var weatherIcon = document.createElement('img');
+          var icon = data.list[0].weather[0].icon;
+          weatherIcon.setAttribute('src', "https://openweathermap.org/img/w/" + icon + ".png");
+          weatherIcon.setAttribute('alt', 'weather icon')
+          resultsDiv.appendChild(weatherIcon);
+
+
+          var fixedCurrentDate = date.split(" ");
           var cityEl = document.createElement("h4");
-          cityEl.style.fontSize = "35px"
-          cityEl.innerText = capitalizeWords(city) + " (" + fixedCurrentDate[0] + ")" + iconUrl;
+          cityEl.style.fontSize = "35px";
+          cityEl.innerText =
+            capitalizeWords(city) + " (" + fixedCurrentDate[0] + ")";
           resultsDiv.appendChild(cityEl);
-          
-          
-          
+
           var tempEl = document.createElement("p");
-          tempEl.innerText = "Current Temp: " + tempF.toFixed(1) + " F";
+          tempEl.innerText = "Current Temp: " + tempF.toFixed(1) + " °F";
           resultsDiv.appendChild(tempEl);
-          
+
           var windEl = document.createElement("p");
           windEl.innerText = "Wind Speed: " + wind + " MPH";
           resultsDiv.appendChild(windEl);
-          
+
           var humidityEl = document.createElement("p");
           humidityEl.innerText = "Humidity Level: " + humidity + "%";
           resultsDiv.appendChild(humidityEl);
-          
-          for (let i = 0; i < 33; i += 8) {
+
+          for (let i = 1; i < 34; i += 8) {
             var tempfuture = data.list[i].main.temp;
             var tempFfuture = ((tempfuture - 273.15) * 9) / 5 + 32;
             var dateforecast = data.list[i].dt_txt;
             var windfuture = data.list[i].wind.speed;
             var humidityfuture = data.list[i].main.humidity;
+
+            var weatherIconfuture = document.createElement('img');
+            var iconfuture = data.list[i].weather[0].icon;
+            weatherIconfuture.setAttribute('src', "https://openweathermap.org/img/w/" + iconfuture + ".png");
+            weatherIconfuture.setAttribute('alt', 'weather icon')
             
-            forecastDiv.style.backgroundColor = "darkblue";
-            forecastDiv.style.color = "white"
-
             var forecast = document.createElement("div");
+            forecast.setAttribute('class', 'forecastDiv')
             forecastDiv.appendChild(forecast);
-
-            var dateEls = document.createElement('p');
+            
+            var dateEls = document.createElement("p");
             var fixedDate = dateforecast.split(" ");
             dateEls.innerText = fixedDate[0];
             forecast.appendChild(dateEls);
+            dateEls.appendChild(weatherIconfuture);
 
             var tempEls = document.createElement("p");
-            tempEls.innerText = "Avg temp: " + tempFfuture.toFixed(2);
+            tempEls.innerText = "Avg temp: " + tempFfuture.toFixed(2) + " °F";
             forecast.appendChild(tempEls);
 
-            var windEls = document.createElement('p');
-            windEls.innerText = "Wind Speed: " + windfuture;
+            var windEls = document.createElement("p");
+            windEls.innerText = "Wind Speed: " + windfuture + " MPH";
             forecast.appendChild(windEls);
 
-            var humidityEls = document.createElement('p');
-            humidityEls.innerText = "Humidity: " + humidityfuture;
+            var humidityEls = document.createElement("p");
+            humidityEls.innerText = "Humidity: " + humidityfuture + " %";
             forecast.appendChild(humidityEls);
           }
         });
@@ -103,8 +111,8 @@ function currentTemp(citySearch) {
 
 buttons.forEach((button) => {
   button.addEventListener("click", function () {
-    resultsDiv.innerHTML = '';
-    forecastDiv.innerHTML = '';
+    resultsDiv.innerHTML = "";
+    forecastDiv.innerHTML = "";
 
     var result = button.value.replace(/ /g, "%");
     currentTemp(result);
@@ -112,8 +120,15 @@ buttons.forEach((button) => {
 });
 
 searchButton.addEventListener("click", function () {
-  forecastDiv.innerHTML = '';
-  resultsDiv.innerHTML = '';
+  forecastDiv.innerHTML = "";
+  resultsDiv.innerHTML = "";
   var city = citySearch.value;
   currentTemp(city);
 });
+
+document.addEventListener('keydown', function(event,city) {
+  if (event.key === 'Enter') {
+    var city = citySearch.value;
+    currentTemp(city);
+  }
+})
